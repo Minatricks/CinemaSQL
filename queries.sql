c29513f4489cdb41da454bfb0d3ca009dfaa37f0
Minatricks <Dimanoit@gmail.com>
2019-05-20 15:35:57 +0300

    Implement full task

--- queries.sql
+++ queries.sql
@@ -10,12 +10,14 @@ GO
 SELECT * FROM Genre INNER JOIN 
 Movie ON Genre.Id_Movie = Movie.Id_Movie
 
+
 Go
+SET STATISTICS TIME OFF 
 --Select movies that have more than two genres
-SELECT COUNT(Id_Genre) AS CountGenresInMovie,Name_Movie FROM 
-Movie INNER JOIN Genre ON Movie.Id_Movie = Genre.Id_Movie
-GROUP BY Name_Movie
-HAVING COUNT(Id_Genre) > 1
+SELECT  COUNT(Genre.Id_Movie) AS CountGenresInMovie,Movie.Name_Movie FROM 
+Genre INNER JOIN Movie ON Genre.Id_Movie = Movie.Id_Movie
+GROUP BY Movie.Name_Movie
+HAVING COUNT(Genre.Id_Movie) > 1
 
 GO
 --Select schedule for today with all movies in all cinema halls and delivery methods specified
@@ -69,54 +71,13 @@ SET IsDeleted = 0
 WHERE Id_ScheduleItem > 0
 
 GO
-CREATE TRIGGER CustomScheduleItemDelete
-ON ScheduleItem
-INSTEAD OF DELETE
-AS
-UPDATE ScheduleItem
-SET IsDeleted = 1
-WHERE ScheduleItem.Id_ScheduleItem =(SELECT deleted.Id_ScheduleItem FROM deleted)
-
-GO
 ENABLE TRIGGER CustomScheduleItemDelete ON ScheduleItem
 
-GO
-CREATE PROCEDURE MakeOrder 
-	@Name NVARCHAR(100),
-	@Phone NVARCHAR(20),
-	@ScheduleItemId INT,
-	@IdHall INT,
-	@IdPlace INT,
-	@SuccessfulOrder BIT OUTPUT
-AS 
-SET @SuccessfulOrder = 0
-DECLARE @IdOrder INT;
-IF ( @IdHall = (SELECT TOP 1 @IdHall FROM Place WHERE Id_Place = @IdPlace) )
-	BEGIN
-	IF( @IdPlace = (SELECT TOP 1 Id_Place FROM Ticket WHERE Id_ScheduleItem = @ScheduleItemId))
-		 PRINT('EROR There is place booked in this hall.');
-	ELSE
-		BEGIN
-		INSERT INTO TicketsOrder(CustomerName,CustomerPhone) VALUES(@Name,@Phone)
-		SET @IdOrder = (SELECT TOP 1 Id_TicketsOrder FROM TicketsOrder ORDER BY Id_TicketsOrder DESC)
-		INSERT INTO Ticket(Id_Place,Id_TicketsOrder,Id_ScheduleItem) VALUES (@IdPlace,@IdOrder,@ScheduleItemId)
-		SET @SuccessfulOrder = 1
-		PRINT('Your order has been successfully created.')
-		END
-	END
-ELSE PRINT('EROR There is no such place in this hall.');
-	 
 GO
 DECLARE @SuccessfulOrder BIT
 EXEC MakeOrder 'Dima Kirieiev','sdfsdf',2,4,220,@SuccessfulOrder OUTPUT
 PRINT(@SuccessfulOrder)
 
 GO
-CREATE VIEW MovieWithShortDescription AS
-SELECT Id_Movie,Name_Movie,Duration,SUBSTRING(Movie.Description, 1, 20) + '...' AS ShortDescription 
-FROM Movie
-
-GO
 SELECT * FROM MovieWithShortDescription
-
 
